af8bc4f95a2209f8722e131fea35e26c6e7642be
projectalgo <y.amulya@gmail.com>
2015-09-05 12:57:16 -0700

    Initial Commit

---
+++ LearningCPPProject/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LearningCPPProject
+//
+//  Created by Yeduguri, Amulya on 9/5/15.
+//  Copyright (c) 2015 Yeduguri, Amulya. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
